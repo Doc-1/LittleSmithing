7746a9aed92f3474eecea6b3448823b27c8c2e64
Doc-1 <kmilligan480@gmail.com>
2020-11-01 23:24:50 -0500

    LittleTransformStucture now edits tiles relative to the structure.

--- main/java/com/littleforge/multitile/strucutres/BrickForgeInteractiveMultiTilePremade.java
+++ main/java/com/littleforge/multitile/strucutres/BrickForgeInteractiveMultiTilePremade.java
@@ -6,6 +6,7 @@ import com.creativemd.littletiles.common.action.LittleActionException;
 import com.creativemd.littletiles.common.action.block.LittleActionActivated;
 import com.creativemd.littletiles.common.structure.registry.LittleStructureType;
 import com.creativemd.littletiles.common.tile.LittleTile;
+import com.creativemd.littletiles.common.tile.math.box.LittleBox;
 import com.creativemd.littletiles.common.tile.parent.IStructureTileList;
 import com.creativemd.littletiles.common.tile.parent.StructureTileList;
 
@@ -13,8 +14,10 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumFacing.Axis;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class BrickForgeInteractiveMultiTilePremade extends InteractiveMultiTilePremade{
@@ -27,10 +30,10 @@ public class BrickForgeInteractiveMultiTilePremade extends InteractiveMultiTileP
 	public boolean onBlockActivated(World worldIn, LittleTile tile, BlockPos pos, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ, LittleActionActivated action)throws LittleActionException {
 		if(worldIn.isRemote)
 			return true;
-		System.out.println("da");
 
-		LittleTransformStructure transform = new LittleTransformStructure(blocksList());
-		
+		//{tiles:[{boxes:[[I;9,1,14,16,4,16],[I;0,1,14,8,4,16],[I;13,5,14,16,8,16],[I;5,5,14,12,8,16],[I;0,5,14,4,8,16]],tile:{color:-11850457,block:"littletiles:ltcoloredblock:5"}},{boxes:[[I;8,1,14,9,4,16],[I;0,0,14,16,1,16],[I;12,5,14,13,8,16],[I;4,5,14,5,8,16],[I;0,4,14,16,5,16]],tile:{color:-11448772,block:"littletiles:ltcoloredblock:5"}},{boxes:[[I;13,13,14,16,16,16],[I;0,13,14,3,16,16],[I;9,9,14,16,12,16],[I;0,9,14,8,12,16],[I;4,13,14,12,16,16]],tile:{color:-2109002457,block:"littletiles:ltcoloredblock:5"}},{boxes:[[I;0,8,14,16,9,16],[I;0,12,14,16,13,16],[I;12,13,14,13,16,16],[I;8,9,14,9,12,16],[I;3,13,14,4,16,16]],tile:{color:-2108600772,block:"littletiles:ltcoloredblock:5"}}],min:[I;0,0,14],size:[I;16,16,2],count:20}
+		LittleTransformStructure transform = new LittleTransformStructure(blocksList(), getSurroundingBox().getAABB());
+		transform.collision();
 		return true;
 	}
 }
--- main/java/com/littleforge/multitile/strucutres/LittleTransformStructure.java
+++ main/java/com/littleforge/multitile/strucutres/LittleTransformStructure.java
@@ -5,11 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.lwjgl.util.Color;
+
 import com.creativemd.creativecore.common.utils.mc.ColorUtils;
 import com.creativemd.littletiles.common.structure.exception.CorruptedConnectionException;
 import com.creativemd.littletiles.common.structure.exception.NotYetConnectedException;
 import com.creativemd.littletiles.common.structure.relative.StructureAbsolute;
 import com.creativemd.littletiles.common.tile.LittleTile;
+import com.creativemd.littletiles.common.tile.math.box.LittleBox;
+import com.creativemd.littletiles.common.tile.math.vec.LittleVec;
 import com.creativemd.littletiles.common.tile.parent.IStructureTileList;
 import com.creativemd.littletiles.common.tile.preview.LittlePreview;
 import com.creativemd.littletiles.common.util.grid.LittleGridContext;
@@ -27,11 +31,24 @@ import net.minecraft.world.World;
 public class LittleTransformStructure {
 	
 	private Iterable<IStructureTileList> blocksList;
-	private Map<BlockPos, List<LittleTile>> tilePosList = new HashMap<BlockPos, List<LittleTile>>();
+	private Map<LittleBox, LittleTile> tilePosList = new HashMap<LittleBox, LittleTile>();
 	private List<LittleTile> tileList;
 	
-	public LittleTransformStructure(Iterable<IStructureTileList> blocksLs) {
+	public AxisAlignedBB absolutePos;
+	public double removeX;
+	public double removeY;
+	public double removeZ;
+	
+	public LittleTransformStructure(Iterable<IStructureTileList> blocksLs, AxisAlignedBB absPos) {
 		blocksList = blocksLs;
+		absolutePos = absPos;
+	
+		
+		
+		removeX = absolutePos.minX;
+		removeY = absolutePos.minY;
+		removeZ = absolutePos.minZ;
+		
 		try {
 			collectAllTiles();
 		} catch (CorruptedConnectionException | NotYetConnectedException e) {
@@ -39,6 +56,7 @@ public class LittleTransformStructure {
 			e.printStackTrace();
 		}
 	}
+	
 	/***
 	 * 
 	 * @param box
@@ -60,24 +78,59 @@ public class LittleTransformStructure {
 			int color = ColorUtils.RGBAToInt(r, g, b, a);
 			NBTTagCompound nbt = new NBTTagCompound();
 	    	littleTile.saveTileExtra(nbt);
+	    	
+	    	
 	    	nbt.setInteger("color", color);
 	    	littleTile.loadTileExtra(nbt);
 		}
 	}
 	
+	public void collision() {
+		LittleBox box = new LittleBox(0, 0, 0, 16, 16, 48);
+		for (LittleBox littleBox : tilePosList.keySet()) {
+			if(LittleBox.intersectsWith(littleBox, box)) {
+				LittleTile littleTile = tilePosList.get(littleBox);
+				int color = ColorUtils.RGBAToInt(25, 0, 0, 255);
+				NBTTagCompound nbt = new NBTTagCompound();
+		    	littleTile.saveTileExtra(nbt);
+		    	if(nbt.hasKey("color")) {
+		    		color = nbt.getInteger("color");
+		    		Color c = ColorUtils.IntToRGBA(color);
+		    		c.setAlpha(255);
+		    		color = ColorUtils.RGBAToInt(c);
+		    	}
+		    	nbt.setInteger("color", color);
+		    	littleTile.loadTileExtra(nbt);
+			}
+			
+		}
+	}
+	/***
+	 * Collects all tiles within the structure. It assigns each tile a box relative to the structure itself.
+	 * Meaning minimum corner of the structure is considered 0,0,0.
+	 */
 	public void collectAllTiles() throws CorruptedConnectionException, NotYetConnectedException {
 		for (IStructureTileList iStructureTileList : blocksList) {
-			System.out.println(iStructureTileList.getPos());
-			iStructureTileList.getTe().updateTiles((x) -> {
-				IStructureTileList list = x.get(iStructureTileList);
+			iStructureTileList.getTe().updateTiles((a) -> {
+				Vec3d relativeMin;
+				Vec3d relativeMax;
+				IStructureTileList list = a.get(iStructureTileList);
 				List<LittleTile> tileLs = new ArrayList<LittleTile>();
+				BlockPos pos = iStructureTileList.getPos();
 				for(LittleTile littleTile : list) {
-					tileLs.add(littleTile);
-					StructureAbsolute absolute = new StructureAbsolute(list.getPos(), littleTile.getBox(), LittleGridContext.get());
+					relativeMin = new Vec3d(((littleTile.getBox().minX/16D) + pos.getX()-removeX), 
+							(littleTile.getBox().minY/16D) + pos.getY()-removeY, 
+							(littleTile.getBox().minZ/16D) + pos.getZ()-removeZ);
+					relativeMax = new Vec3d(((littleTile.getBox().maxX/16D) + pos.getX()-removeX), 
+							(littleTile.getBox().maxY/16D) + pos.getY()-removeY, 
+							(littleTile.getBox().maxZ/16D) + pos.getZ()-removeZ);
+					
+					LittleBox relativeBox = new LittleBox(new LittleVec((int) (relativeMin.x*16), (int) (relativeMin.y*16), (int) (relativeMin.z*16)), 
+							new LittleVec((int) (relativeMax.x*16), (int) (relativeMax.y*16), (int) (relativeMax.z*16)));
+					//System.out.println(relativeMin + " " + relativeMax + " " + relativeBox);
 
-					System.out.println(littleTile.getBox() +" "+ list.getPos()); 
+					tilePosList.put(relativeBox, littleTile);
 				}
-				tilePosList.put(iStructureTileList.getPos(), tileLs);
 			});
 		}
 		
