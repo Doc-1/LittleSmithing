1fc132873d6da1077e39cb32654073ebad7b47ba
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-01-19 01:43:21 +0100

    Fixed couple of rendering issues

--- main/java/com/littleforge/common/item/placeable/weapon/PremadePlaceableItemWeapon.java
+++ main/java/com/littleforge/common/item/placeable/weapon/PremadePlaceableItemWeapon.java
@@ -40,162 +40,160 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class PremadePlaceableItemWeapon extends ItemSword implements ILittleTile, ICreativeRendered {
-	
-	public String premadeToPlace = "";
-	public String premadeToRender = "";
-	public boolean isShifting = false;
-	
-	public PremadePlaceableItemWeapon(ToolMaterial material, String unlocalizedName, String registryName, String premadeToRender, String premadeToPlace) {
-		super(material);
-		this.premadeToPlace = premadeToPlace;
-		this.premadeToRender = premadeToRender;
-		setUnlocalizedName(unlocalizedName);
-		setRegistryName(registryName);
-		hasSubtypes = true;
-	}
-	
-	public ActionResult<ItemStack> onRightClickEntity(ItemStack heldItem, EntityPlayer player, EntityLivingBase entity) {
-		return null;
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public List<RenderBox> getRenderingCubes(IBlockState state, TileEntity te, ItemStack stack) {
-		LittleStructureTypePremade premade = (LittleStructureTypePremade) LittleStructureRegistry.getStructureType(premadeToRender);
-		LittlePreviews previews = LittleStructurePremade.getPreviews(premade.id).copy();
-		List<RenderBox> cubes = premade.getRenderingCubes(previews);
-		if (cubes == null) {
-			cubes = new ArrayList<>();
-			
-			for (LittlePreview preview : previews.allPreviews())
-				cubes.add(preview.getCubeBlock(previews.getContext()));
-		}
-		return cubes;
-	}
-	
-	@SideOnly(Side.CLIENT)
-	public static IBakedModel model;
-	
-	@Override
-	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-		isShifting = entityIn.isSneaking();
-	}
-	
-	@Override
-	public boolean onRightClick(World world, EntityPlayer player, ItemStack stack, PlacementPosition position, RayTraceResult result) {
-		if (player.isSneaking()) {
-			return true;
-		}
-		return false;
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public LittleGridContext getPositionContext(ItemStack stack) {
-		return ItemMultiTiles.currentContext;
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public SubGuiConfigure getConfigureGUIAdvanced(EntityPlayer player, ItemStack stack) {
-		return new SubGuiModeSelector(stack, ItemMultiTiles.currentContext, ItemLittleChisel.currentMode) {
-			
-			@Override
-			public void saveConfiguration(LittleGridContext context, PlacementMode mode) {
-				ItemLittleChisel.currentMode = mode;
-				ItemMultiTiles.currentContext = context;
-			}
-		};
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void saveCachedModel(EnumFacing facing, BlockRenderLayer layer, List<BakedQuad> cachedQuads, IBlockState state, TileEntity te, ItemStack stack, boolean threaded) {
-		stack = LittleStructurePremade.getPremadeStack(premadeToRender);
-		if (stack != null)
-			ItemModelCache.cacheModel(getPremade(stack).stack, facing, cachedQuads);
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public List<BakedQuad> getCachedModel(EnumFacing facing, BlockRenderLayer layer, IBlockState state, TileEntity te, ItemStack stack, boolean threaded) {
-		stack = LittleStructurePremade.getPremadeStack(premadeToRender);
-		if (stack == null)
-			return null;
-		LittleStructurePremadeEntry entry = getPremade(stack);
-		if (entry == null)
-			return null;
-		return ItemModelCache.requestCache(entry.stack, facing);
-	}
-	
-	@Override
-	public boolean hasLittlePreview(ItemStack stack) {
-		return isShifting;
-	}
-	
-	public void removeUnnecessaryData(ItemStack stack) {
-		if (stack.hasTagCompound()) {
-			stack.getTagCompound().removeTag("tiles");
-			stack.getTagCompound().removeTag("size");
-			stack.getTagCompound().removeTag("min");
-		}
-	}
-	
-	private static HashMap<String, LittlePreviews> cachedPreviews = new HashMap<>();
-	
-	public static void clearCache() {
-		cachedPreviews.clear();
-	}
-	
-	@Override
-	public LittlePreviews getLittlePreview(ItemStack stack) {
-		String id = getPremadeId(LittleStructurePremade.tryGetPremadeStack(this.premadeToPlace));
-		if (cachedPreviews.containsKey(id))
-			return cachedPreviews.get(id).copy();
-		return LittleStructurePremade.getPreviews(id).copy();
-	}
-	
-	@Override
-	public void saveLittlePreview(ItemStack stack, LittlePreviews previews) {
-		cachedPreviews.put(getPremadeId(LittleStructurePremade.tryGetPremadeStack(this.premadeToPlace)), previews);
-	}
-	
-	@Override
-	public boolean sendTransformationUpdate() {
-		return false;
-	}
-	
-	@Override
-	public boolean containsIngredients(ItemStack stack) {
-		return true;
-	}
-	
-	@Override
-	public PlacementMode getPlacementMode(ItemStack stack) {
-		if (!ItemMultiTiles.currentMode.canPlaceStructures())
-			return PlacementMode.getStructureDefault();
-		return ItemMultiTiles.currentMode;
-	}
-	
-	@Override
-	public boolean shouldCache() {
-		return false;
-	}
-	
-	@Override
-	public boolean snapToGridByDefault() {
-		return true;
-	}
-	
-	public static String getPremadeId(ItemStack stack) {
-		if (stack.hasTagCompound())
-			return stack.getTagCompound().getCompoundTag("structure").getString("id");
-		return null;
-	}
-	
-	public static LittleStructurePremadeEntry getPremade(ItemStack stack) {
-		if (stack.hasTagCompound())
-			return LittleStructurePremade.getStructurePremadeEntry(stack.getTagCompound().getCompoundTag("structure").getString("id"));
-		return null;
-	}
+    
+    public String premadeToPlace = "";
+    public String premadeToRender = "";
+    public boolean isShifting = false;
+    
+    public PremadePlaceableItemWeapon(ToolMaterial material, String unlocalizedName, String registryName, String premadeToRender, String premadeToPlace) {
+        super(material);
+        this.premadeToPlace = premadeToPlace;
+        this.premadeToRender = premadeToRender;
+        setUnlocalizedName(unlocalizedName);
+        setRegistryName(registryName);
+        hasSubtypes = true;
+    }
+    
+    public ActionResult<ItemStack> onRightClickEntity(ItemStack heldItem, EntityPlayer player, EntityLivingBase entity) {
+        return null;
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public List<RenderBox> getRenderingCubes(IBlockState state, TileEntity te, ItemStack stack) {
+        LittleStructureTypePremade premade = (LittleStructureTypePremade) LittleStructureRegistry.getStructureType(premadeToRender);
+        LittlePreviews previews = LittleStructurePremade.getPreviews(premade.id).copy();
+        List<RenderBox> cubes = premade.getRenderingCubes(previews);
+        if (cubes == null) {
+            cubes = new ArrayList<>();
+            
+            for (LittlePreview preview : previews.allPreviews())
+                cubes.add(preview.getCubeBlock(previews.getContext()));
+            
+            LittlePreview.shrinkCubesToOneBlock(cubes);
+        }
+        return cubes;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public static IBakedModel model;
+    
+    @Override
+    public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        isShifting = entityIn.isSneaking();
+    }
+    
+    @Override
+    public boolean onRightClick(World world, EntityPlayer player, ItemStack stack, PlacementPosition position, RayTraceResult result) {
+        if (player.isSneaking()) {
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public LittleGridContext getPositionContext(ItemStack stack) {
+        return ItemMultiTiles.currentContext;
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public SubGuiConfigure getConfigureGUIAdvanced(EntityPlayer player, ItemStack stack) {
+        return new SubGuiModeSelector(stack, ItemMultiTiles.currentContext, ItemLittleChisel.currentMode) {
+            
+            @Override
+            public void saveConfiguration(LittleGridContext context, PlacementMode mode) {
+                ItemLittleChisel.currentMode = mode;
+                ItemMultiTiles.currentContext = context;
+            }
+        };
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void saveCachedModel(EnumFacing facing, BlockRenderLayer layer, List<BakedQuad> cachedQuads, IBlockState state, TileEntity te, ItemStack stack, boolean threaded) {
+        stack = LittleStructurePremade.getPremadeStack(premadeToRender);
+        if (stack != null)
+            ItemModelCache.cacheModel(getPremade(stack).stack, facing, cachedQuads);
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public List<BakedQuad> getCachedModel(EnumFacing facing, BlockRenderLayer layer, IBlockState state, TileEntity te, ItemStack stack, boolean threaded) {
+        if (stack == null)
+            return null;
+        return ItemModelCache.requestCache(stack, facing);
+    }
+    
+    @Override
+    public boolean hasLittlePreview(ItemStack stack) {
+        return isShifting;
+    }
+    
+    public void removeUnnecessaryData(ItemStack stack) {
+        if (stack.hasTagCompound()) {
+            stack.getTagCompound().removeTag("tiles");
+            stack.getTagCompound().removeTag("size");
+            stack.getTagCompound().removeTag("min");
+        }
+    }
+    
+    private static HashMap<String, LittlePreviews> cachedPreviews = new HashMap<>();
+    
+    public static void clearCache() {
+        cachedPreviews.clear();
+    }
+    
+    @Override
+    public LittlePreviews getLittlePreview(ItemStack stack) {
+        String id = getPremadeId(LittleStructurePremade.tryGetPremadeStack(this.premadeToPlace));
+        if (cachedPreviews.containsKey(id))
+            return cachedPreviews.get(id).copy();
+        return LittleStructurePremade.getPreviews(id).copy();
+    }
+    
+    @Override
+    public void saveLittlePreview(ItemStack stack, LittlePreviews previews) {
+        cachedPreviews.put(getPremadeId(LittleStructurePremade.tryGetPremadeStack(this.premadeToPlace)), previews);
+    }
+    
+    @Override
+    public boolean sendTransformationUpdate() {
+        return false;
+    }
+    
+    @Override
+    public boolean containsIngredients(ItemStack stack) {
+        return true;
+    }
+    
+    @Override
+    public PlacementMode getPlacementMode(ItemStack stack) {
+        if (!ItemMultiTiles.currentMode.canPlaceStructures())
+            return PlacementMode.getStructureDefault();
+        return ItemMultiTiles.currentMode;
+    }
+    
+    @Override
+    public boolean shouldCache() {
+        return false;
+    }
+    
+    @Override
+    public boolean snapToGridByDefault() {
+        return true;
+    }
+    
+    public static String getPremadeId(ItemStack stack) {
+        if (stack.hasTagCompound())
+            return stack.getTagCompound().getCompoundTag("structure").getString("id");
+        return null;
+    }
+    
+    public static LittleStructurePremadeEntry getPremade(ItemStack stack) {
+        if (stack.hasTagCompound())
+            return LittleStructurePremade.getStructurePremadeEntry(stack.getTagCompound().getCompoundTag("structure").getString("id"));
+        return null;
+    }
 }
